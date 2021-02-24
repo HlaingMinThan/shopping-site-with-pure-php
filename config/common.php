bd18351533d96f734078fdc3dcc7a2186b28727d
HlaingMinThan <hlaingminthan92@gmail.com>
2021-02-24 16:59:42 +0630

    set up admin template and start working with products & categories pages
     add search functions in those both pages and add auth system in admin panel

---
+++ config/common.php
@@ -0,0 +1,32 @@
+<?php 
+    session_start();
+   
+
+    if($_SERVER['REQUEST_METHOD']==='POST'){
+        if(!hash_equals($_SESSION['_token'],$_POST['_token'])){
+            echo "invalid request for csrf attack";
+            die(); //some error template should  show
+        }else{
+            unset($_SESSION['_token']);
+        }
+    }
+    /* this code should place below 
+    check post request code because 
+    after unset session we need another token session for other post request */
+    if (empty($_SESSION['_token'])) {
+        if (function_exists('random_bytes')) {
+            $_SESSION['_token'] = bin2hex(random_bytes(32));
+        } else {
+            $_SESSION['_token'] = bin2hex(openssl_random_pseudo_bytes(32));
+        }
+    }
+
+        /**
+     * Escapes HTML for output
+     *
+     */
+
+    function escape($html) {
+        return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
+    }
+?>
