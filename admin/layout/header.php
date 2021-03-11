ec83621fe305edf4c95db36066dbd6cb8dbae2b3
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-03-11 15:16:38 +0630

    adding royal users report in admin panel

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -155,7 +155,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="royal_users.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Royal Users</p>
                 </a>
